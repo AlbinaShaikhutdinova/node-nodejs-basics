8957dccde6677c897641c850ac3ff65a57c8e22a
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2022-12-03 19:33:12 +0300

    feat: complete zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const newFilePath = path.join(__dirname, 'files', 'archive.gz');
+  const input = fs.createReadStream(filePath, 'utf-8');
+  const output = fs.createWriteStream(newFilePath);
+  const gzip = zlib.createGzip();
+
+  pipeline(input, gzip, output, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 };
 
 await compress();
