2aa7c5da911bacec9eac97f322378337c6274f1a
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2022-12-03 19:21:10 +0300

    feat: complete stream module

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
 const read = async () => {
-    // Write your code here 
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const stream = fs.createReadStream(filePath, 'utf-8');
+
+  let data = '';
+
+  stream.on('data', (chunk) => (data += chunk));
+  stream.on('end', () => process.stdout.write(data));
+  stream.on('error', (error) => console.log('Error', error.message));
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
 const write = async () => {
-    // Write your code here 
+  const { stdin, stdout } = process;
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const output = fs.createWriteStream(filePath);
+  stdout.write('Write anything:\n');
+  stdin.on('data', (data) => {
+    output.write(data);
+  });
 };
 
 await write();
