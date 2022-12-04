57d891889c8cbb07a23b741f43029bf25de3305c
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2022-12-04 14:44:03 +0300

    feat: complete cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import url from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = url.fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'script.js');
+  const child = spawn('node', [filePath]);
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 spawnChildProcess();
